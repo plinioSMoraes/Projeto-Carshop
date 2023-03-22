7080f6fd8733c23ee4f3fd768a68f86a043e9f88
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2023-03-22 17:40:49 -0500

    Error de lint no server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import connectToDatabase from './Models/Connection';
 const PORT = process.env.PORT || 3001;
 
 connectToDatabase()
-    .then(() => {
-        app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
-    })
-    .catch((error) => {
-        console.log('Connection with database generated an error:\r\n');
-        console.error(error);
-        console.log('\r\nServer initialization cancelled');
-        process.exit(0);
-    });
+  .then(() => {
+    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log('Connection with database generated an error:\r\n');
+    console.error(error);
+    console.log('\r\nServer initialization cancelled');
+    process.exit(0);
+  });
